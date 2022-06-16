7f2ff10ee302344a97caa75c30ccc0c80810ed89
Yarixa Rodriguez <yarixarodriguez94@gmail.com>
2022-06-15 21:30:36 -0700

    Done with assignment

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const h2 = document.createElement("h2")
-//h2.textContent = "This content added by JavaScript";
+let addToCartButton = document.getElementById('addToCart');
+let addToContainer = document.getElementById('addToContainer');
+let inputField = document.getElementById('inputField');
 
-document.querySelector("body").appendChild(h2);
+addToCartButton.addEventListener('click', function(){
+    const paragraph = document.createElement('p')
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    addToContainer.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener('click', function() {
+        paragraph.style.textDecoration = "line-through";
+    })
+    })
+  
